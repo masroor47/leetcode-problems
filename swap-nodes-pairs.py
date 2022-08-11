bb373900d39f4b8ae09bd6ccde6ffbeeac43df24
Masroor Khonkhodzhaev <masroor47@icloud.com>
2022-08-11 11:40:04 -0400

    'swap nodes in pairs' now works, but slow

--- swap-nodes-pairs.py
+++ swap-nodes-pairs.py
@@ -1,6 +1,9 @@
 from node import ListNode
 
-item4 = ListNode(4)
+item7 = ListNode(7)
+item6 = ListNode(6, item7)
+item5 = ListNode(5, item6)
+item4 = ListNode(4, item5)
 item3 = ListNode(3, item4)
 item2 = ListNode(2, item3)
 head = ListNode(1, item2)
@@ -17,12 +20,7 @@ def printList(head):
 print(printList(head))
 
 
-
-
-
-
-
-
+# ====== Works, but slow. Recursive might be better. ======
 def swapPairs(head):
     if head is None:
         return None
@@ -32,28 +30,31 @@ def swapPairs(head):
     prev = head
     current = prev.next
     head = current
-    print(prev.val, current.val)
-    
+
+    if current.next is None:
+        current.next = prev
+        prev.next = None
+        return head
+
     while current:
         # print("Swapping {} and {}".format(prev.val, current.val))
         if current.next:
             temp = current.next
             current.next = prev
-            prev.next = temp
+            if temp.next:
+                prev.next = temp.next
+            else:
+                prev.next = temp
         else:
             print("hit the end so to speak")
             current.next = prev
             prev.next = None
 
-        print(f"prev: {prev}, current: {current} before advancing")
+        print(f"prev: {prev}, current: {current} after relinking")
         print("list after relinking: " + printList(head))
         
-        
-        prev = prev.next
-        if prev:
-            current = prev.next
-        else:
-            print("Not advancing current, prev is None")
+        prev = temp
+        current = temp.next
     
         print(f"prev: {prev}, current: {current} after advancing")
         print("\n")
