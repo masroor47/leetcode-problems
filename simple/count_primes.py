321217eedbb96affc2e4aae9edcdd72b4fe34b1d
Masroor Khonkhodzhaev <masroor47@icloud.com>
2022-09-04 15:46:18 -0400

    count primes solution accepted, but unreliable

--- simple/count_primes.py
+++ simple/count_primes.py
@@ -8,26 +8,44 @@ import math
 
 def count_primes(n: int) -> int:
 
-    count = 0
-    if n <= 1:
+    # count = 0
+    # if n <= 1:
+    #     return 0
+    # for i in range(2, n):
+    #     sqrt = math.floor(math.sqrt(i))
+    #     print(f"i: {i},    sqrt: {sqrt}")
+    #     isprime = True
+        
+    #     for j in range(2, sqrt+1):
+    #         print(f"j: {j}")
+    #         if i % j == 0:
+    #             print(f"{i} is not prime")
+    #             isprime = False
+    #             break
+        
+    #     if isprime:
+    #         print(f"{i} is prime (?)")
+    #         count += 1
+    #     print()
+    # return count
+    if primes < 3:
         return 0
-    for i in range(2, n):
-        sqrt = math.floor(math.sqrt(i))
-        print(f"i: {i},    sqrt: {sqrt}")
-        isprime = True
-        
-        for j in range(2, sqrt+1):
-            print(f"j: {j}")
-            if i % j == 0:
-                print(f"{i} is not prime")
-                isprime = False
-                break
-        
-        if isprime:
-            print(f"{i} is prime (?)")
-            count += 1
-        print()
+    primes = [True] * n
+    primes[0], primes[1] = False, False
+    for i in range(2, len(primes)):
+        if primes[i]:
+            sq = i**2
+            for j in range(sq, n, i):
+                primes[j] = False
+    print(primes)
+    count = 0
+    for i in primes:
+        if i:
+            count+=1
+
     return count
+
+
 
 
 
