b65d2de34998cb3dada383908ba94fbf9461bb4a
Masroor Khonkhodzhaev <masroor47@icloud.com>
2022-09-07 17:18:45 -0400

    find k largest in array works, but time limit exceeded. need O(n) instead of O(n*k)

---
+++ simple/find_k_largest_in_array.py
@@ -0,0 +1,26 @@
+
+
+
+
+def find_k_largest(nums: list[int], k: int) -> int:
+    
+    k_items = []
+
+    for i in nums:
+        is_largest = True
+        for j in range(len(k_items)):
+            if i < k_items[j]:
+                k_items.insert(j, i)
+                is_largest = False
+                break
+        if is_largest:
+            k_items.append(i)
+        
+        if len(k_items) > k:
+            k_items.pop(0)
+        print(f"items: {k_items}, {i}")
+    
+    return k_items[0]
+
+
+print(find_k_largest([3,2,3,1,2,4,5,5,6], 4))
