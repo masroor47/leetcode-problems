4a1e9d6854bba72808c16a7725e78a2cdd093842
Masroor Khonkhodzhaev <masroor47@icloud.com>
2022-09-08 09:00:23 -0400

    works faster with python built-in sorting lol

--- simple/find_k_largest_in_array.py
+++ simple/find_k_largest_in_array.py
@@ -3,24 +3,29 @@
 
 
 def find_k_largest(nums: list[int], k: int) -> int:
-    
-    k_items = []
+    nums.sort()
+    return nums[-k]
 
-    for i in nums:
-        is_largest = True
-        for j in range(len(k_items)):
-            if i < k_items[j]:
-                k_items.insert(j, i)
-                is_largest = False
-                break
-        if is_largest:
-            k_items.append(i)
+    # stupid ass question. it's very easy in python
+
+
+    # k_items = []
+
+    # for i in nums:
+    #     is_largest = True
+    #     for j in range(len(k_items)):
+    #         if i < k_items[j]:
+    #             k_items.insert(j, i)
+    #             is_largest = False
+    #             break
+    #     if is_largest:
+    #         k_items.append(i)
         
-        if len(k_items) > k:
-            k_items.pop(0)
-        print(f"items: {k_items}, {i}")
+    #     if len(k_items) > k:
+    #         k_items.pop(0)
+    #     print(f"items: {k_items}, {i}")
     
-    return k_items[0]
+    # return k_items[0]
 
 
 print(find_k_largest([3,2,3,1,2,4,5,5,6], 4))
